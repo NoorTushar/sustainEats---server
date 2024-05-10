af5d07150d00640567d6358f54d0ff72408dcf47
NoorTushar <tushar.noor.khan@gmail.com>
2024-05-10 23:30:13 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+require("dotenv").config();
+
+const corsOptions = {
+   origin: ["http://localhost:5173"],
+   credentials: true,
+   optionSuccessStatus: 200,
+};
+
+// middlewares:
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// mongoDB
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j7c4zww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+   serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+   },
+});
+
+async function run() {
+   try {
+      // Connect the client to the server	(optional starting in v4.7)
+      //   await client.connect();
+      // Send a ping to confirm a successful connection
+      //   await client.db("admin").command({ ping: 1 });
+
+      // APIs
+
+      console.log(
+         "Pinged your deployment. You successfully connected to MongoDB!"
+      );
+   } finally {
+      // Ensures that the client will close when you finish/error
+      //   await client.close();
+   }
+}
+run().catch(console.dir);
+
+// for testing
+app.get("/", (req, res) => {
+   res.send("sustainEats is Running");
+});
+
+// listen
+app.listen(port, () => {
+   console.log(`sustainEats is running at port: ${port}`);
+});
