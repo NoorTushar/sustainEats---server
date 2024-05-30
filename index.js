277e58445f8fad1c71ab65f21998a93566038acb
NoorTushar <tushar.noor.khan@gmail.com>
2024-05-30 16:00:07 +0600

    get all payment route is showing but not proteced by verify token. have to do it

--- index.js
+++ index.js
@@ -276,6 +276,17 @@ async function run() {
 
       /****** Payment APIs *******/
 
+      // get all payments from db
+      app.get("/payments/:email", async (req, res) => {
+         const query = { email: req.params.email };
+
+         // if (req.params.email !== req.decoded.email) {
+         //    return res.status(403).send({ message: "forbidden Access" });
+         // }
+         const result = await paymentCollection.find(query).toArray();
+         res.send(result);
+      });
+
       // payment (2)
       app.post("/create-payment-intent", async (req, res) => {
          try {
